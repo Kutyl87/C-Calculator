ccef272e32b6d45b59905facd426a3daada5b4c9
pkutyl <01168894@pw.edu.pl>
2024-02-21 16:16:46 +0100

    Add tests for basic calc operations

--- lib/calculator.cpp
+++ lib/calculator.cpp
@@ -7,4 +7,4 @@ double Calculator::add(double first_number, double second_number) {
 double Calculator::subtract(double first_number, double second_number) {
   return first_number - second_number;
 }
-bool Calculator::getStatus() { return isRunning; }
+bool Calculator::getStatus() { return isRunning;}
--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -1,9 +1,22 @@
 #include "calculator.h"
 #include <gtest/gtest.h>
 
-TEST(BasicTest, TestPass) {
+TEST(CalculatorTests, testStatus) {
   Calculator calc = Calculator();
   EXPECT_EQ(calc.getStatus(), false);
+}
+TEST(CalculatorTests, testPressRun) {
+  Calculator calc = Calculator();
+  calc.pressRun();
+  EXPECT_EQ(calc.getStatus(), true);
+}
+TEST(CalculatorTests, testAdd) {
+  Calculator calc = Calculator();
+  EXPECT_EQ(calc.add(1, 2), 3);
+}
+TEST(CalculatorTests, testSubtract) {
+  Calculator calc = Calculator();
+  EXPECT_EQ(calc.subtract(2, 1), 1);
 }
 
 int main(int argc, char **argv) {
